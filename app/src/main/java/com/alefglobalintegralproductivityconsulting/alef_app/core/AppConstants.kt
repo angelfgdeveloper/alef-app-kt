1ddc23fc5fd69d2f0d0631151a23f271b35bf582
Angel <angelfgdeveloper@gmail.com>
2022-02-02 12:50:44 -0600

    SplashScreen terminado

--- app/src/main/java/com/alefglobalintegralproductivityconsulting/alef_app/core/AppConstants.kt
+++ app/src/main/java/com/alefglobalintegralproductivityconsulting/alef_app/core/AppConstants.kt
@@ -2,8 +2,11 @@ package com.alefglobalintegralproductivityconsulting.alef_app.core
 
 class AppConstants {
     companion object {
+        const val IS_CLOSED_PREVIEW_TIME: String = "IS_CLOSED_PREVIEW_TIME"
+
         const val IS_LOGIN_USER: String = "IS_LOGIN_USER"
         const val USER_TOKEN: String = "USER_TOKEN"
+
         const val DETAILS_VACANT: String = "DETAILS_VACANT"
     }
 }
