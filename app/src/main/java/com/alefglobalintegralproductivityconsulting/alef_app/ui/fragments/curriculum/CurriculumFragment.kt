1a40d9d2aa0bb666495e014f6b3c3ac6644fcb75
Angel <angelfgdeveloper@gmail.com>
2022-02-04 13:56:01 -0600

    Boton para hacer visible o invisible el curriculum

--- app/src/main/java/com/alefglobalintegralproductivityconsulting/alef_app/ui/fragments/curriculum/CurriculumFragment.kt
+++ app/src/main/java/com/alefglobalintegralproductivityconsulting/alef_app/ui/fragments/curriculum/CurriculumFragment.kt
@@ -2,6 +2,8 @@ package com.alefglobalintegralproductivityconsulting.alef_app.ui.fragments.curri
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.alefglobalintegralproductivityconsulting.alef_app.R
 import com.alefglobalintegralproductivityconsulting.alef_app.databinding.FragmentCurriculumBinding
@@ -13,6 +15,20 @@ class CurriculumFragment : Fragment(R.layout.fragment_curriculum) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mBinding = FragmentCurriculumBinding.bind(view)
+
+        mBinding.chipIsVisible.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+//                mBinding.chipIsVisible.checkedIcon =
+//                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_success)
+//                mBinding.chipIsVisible.checkedIconTint =
+//                    AppCompatResources.getColorStateList(requireContext(), R.color.purple_500)
+                mBinding.chipIsVisible.text = "Perfil: Visible"
+            } else {
+//                mBinding.chipIsVisible.checkedIcon =
+//                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_state_circle)
+                mBinding.chipIsVisible.text = "Perfil: Invisible"
+            }
+        }
     }
 
 }
